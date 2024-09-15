63915ba163cac99c480d9b7fef515e6a41a8ee6d
Satwik Krishna <135621291+satwikkrshna@users.noreply.github.com>
2024-09-15 03:25:02 -0700

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
       'Trademarkia',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'VIT 21BCT0140 Trademarkia',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
